955899d41042a1dbc07a77933b739a1313e453dc
Dan Chaltiel <dan.chaltiel@gmail.com>
2022-11-04 21:03:04 +0100

    update options manual

--- R/options.R
+++ R/options.R
@@ -1,25 +1,14 @@
 
-#TODO edc_options()
-edc_options = function(trialmaster_pw, .local=FALSE){
-  
-  argg = as.list(match.call())[-1]
-  
-  
-  dup = names(argg) %in% paste0("crosstable_", names(argg))
-  dup_args = names(argg)[dup]
-  argg = argg[!names(argg) %in% dup_args]
-  prefix = ifelse(str_starts(names(argg), "crosstable_"), "", "crosstable_")
-  names(argg) = paste0(prefix, names(argg))
-  if(length(dup_args)>0){
-    cli::cli_warn(c("Duplicated crosstable option{?s} were ignored: {dup_args}.",
-                    i='You can now remove the old "crosstable_" prefix.'),
-                  class="crosstable_dupl_option_warning")
-  }
-  
-  if(.local){
-    argg = c(argg, .frame = caller_env())
-    do.call(local_options, argg)
-  }
-  else do.call(options, argg)
-  invisible()
-}
+
+
+#' Options used in `EDCimport`
+#'
+#' @description
+#' Here are all the options used in `EDCimport`:
+#' 
+#'  * `trialmaster_pw`: the password for the archive read by [read_trialmaster()]
+#'  * `edc_lookup`: the default data used by [find_keyword()]
+#'  * `path_7zip`: the path to 7-zip directory if not in the PATH, usually "C:/Program Files/7-Zip/" on Windows. See [extract_7z()] for more information.
+#'
+#' @name EDCimport_options
+NULL
