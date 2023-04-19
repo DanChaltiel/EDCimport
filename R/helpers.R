54df570b0207cb4ba23020064546434d09f8d867
Dan Chaltiel <dan.chaltiel@gmail.com>
2023-04-19 17:53:36 +0200

    improve swimmerplor

--- R/helpers.R
+++ R/helpers.R
@@ -183,36 +183,85 @@ save_list = function(x, filename){
 
 #' Get a swimmer plot of all dates in the database
 #'
-#' @param .lookup the result of [get_lookup()]
+#' @param .lookup the lookup table, loaded along with the database or result of [get_lookup()]
 #' @param id an identifier for a given row
-#' @param plotly whether to use {plotly} to get an interactive plot
+#' @param origin a variable to consider as the time 0, given as "dataset$column"
+#' @param aes_color either `variable` ("{dataset} - {column}") or `label` (column label)
+#' @param plotly whether to use `{plotly}` to get an interactive plot
 #'
 #' @return a plot
 #' @export
-swimmerplot = function(.lookup=getOption("edc_lookup", NULL), id="SUBJID", plotly=TRUE){
+swimmerplot = function(.lookup=getOption("edc_lookup", NULL), id="SUBJID", 
+                       origin=NULL, 
+                       aes_color=c("variable", "label"), plotly=TRUE){
   check_installed("ggplot2", reason="for `swimmerplot()` to work.")
+  aes_color = match.arg(aes_color)
   parent = parent.frame()
   
-  x = .lookup$dataset %>%
+  dbs = .lookup$dataset %>%
     set_names() %>% 
-    map(~{
-      dat = get(.x, envir=parent)
-      if(!id %in% names(dat)) return(NULL)
-      a = dat %>% 
-        select(id=!!id, where(is.Date))
-      if(ncol(a)<2) return(NULL)
-      a %>% 
-        pivot_longer(-id) %>% 
-        mutate(label=unlist(var_label(a)[name]) %||% name,
-               variable=paste0(toupper(.x), " - ", toupper(name)))
-    })
+    map(~get(.x, envir=parent))
+  if(length(dbs)==0){
+    cli_abort("Unexpected error, contact the developper")
+  }
   
-  p = list_rbind(x) %>% 
-    mutate(id=factor(id)) %>% 
-    ggplot2::ggplot(ggplot2::aes(x=value, y=id, color=label, group=id, label=variable)) + 
-    ggplot2::geom_line() +
-    ggplot2::geom_point() +
-    ggplot2::labs(x=NULL, y=id, color="Variable")
+  dbs = dbs %>% 
+    discard(~!id %in% names(.x))
+  if(length(dbs)==0){
+    cli_abort(c("None of the datasets contains an identifier column", i="{.arg id}={.val {id}}"))
+  }
+  
+  dbs = dbs %>% 
+    map(~.x %>% select(id=!!id, where(is.Date))) %>% 
+    discard(~ncol(.x)<2)
+  if(length(dbs)==0){
+    cli_abort(c("None of the datasets contains a date column"))
+  }
+  
+  dat = dbs %>% 
+    imap(~{
+      .x %>% 
+        pivot_longer(-id) %>% 
+        mutate(label=unlist(var_label(.x)[name]) %||% name,
+               dataset=.y,
+               variable=paste0(toupper(dataset), " - ", toupper(name)))
+    }) %>% 
+    list_rbind() %>% 
+    mutate(id=factor(id))
+  
+  x_label = "Calendar date"
+  # origin="enrolres$enrolldt"
+  if(!is.null(origin)){
+    if(!str_detect(origin, "^.*\\$.*$")){
+      cli_abort("{.arg origin} is not in the form `dataset$column`")
+    }
+    origin2 = str_split(origin, "\\$", 2)[[1]]
+    dat_origin = dat %>% 
+      filter(dataset==origin2[1], name==origin2[2]) %>% 
+      select(id, origin=value)
+    if(nrow(dat_origin)==0){
+      cli_abort(c("{.arg origin} is wrong somehow", i="sorry for the unhelpful message"))
+    }
+    dat = dat %>% 
+      left_join(dat_origin, by="id") %>% 
+      mutate(
+        value_bak = value,
+        value = as.numeric(value-origin)
+      )
+    x_label = glue("Date difference from `{origin}` (in days)")
+  }
+  
+  aes_label = "variable"
+  if(aes_color=="variable"){ 
+    aes_label = "label"
+  }
+  
+  p = dat %>% 
+    ggplot2::ggplot(ggplot2::aes(x=value, y=id, group=id)) + 
+    ggplot2::aes(color=!!sym(aes_color), label=!!sym(aes_label)) +
+    ggplot2::geom_line(na.rm=TRUE) +
+    ggplot2::geom_point(na.rm=TRUE) +
+    ggplot2::labs(x=x_label, y=id, color="Variable")
   
   if(isTRUE(plotly)){
     check_installed("plotly", reason="for `swimmerplot(plotly=TRUE)` to work.")
