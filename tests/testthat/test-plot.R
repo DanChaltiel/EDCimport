123d846a37196bf32e213afe5825976b3bc5cf8b
Dan Chaltiel <dan.chaltiel@gmail.com>
2023-05-12 15:31:55 +0200

    Update test-plot.R

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -4,23 +4,8 @@ skip_on_cran()
 
 
 test_that("swimmerplot", {
-  set.seed(42)
-  N = 50
-  start = ISOdate(2010, 04, 13)
-  day=3600*24
-  db = tibble(SUBJID=1:N, age=rnorm(N, 50, 10), date_naissance=start-age*day)
-  
-  for(i in 1:10){
-    db[[paste0("date",i)]] = start+rnorm(N, i*10, 10)*day 
-  }
-  
-  db0=db %>% select(SUBJID, 1:3) %>% mutate(group=ifelse(runif(n())>0.5, "A", "B"))
-  db1=db %>% select(SUBJID, 4:6) %>% mutate(x=ifelse(runif(n())>0.5, "X", "Y"))
-  db1=bind_rows(db1, db1)
-  db2=db %>% select(SUBJID, 7:9)
-  db3=db %>% select(SUBJID, 10:13)
-  
-  .lookup = tibble(dataset=paste0("db", 0:3))
+  e = edc_example_plot()
+  load_list(e)
   
   p = swimmerplot(.lookup, plotly=FALSE)
   vdiffr::expect_doppelganger("swimmerplot", p)
