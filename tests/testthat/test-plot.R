785ee4dbbc48129c12957ac6f706ab61eed579c4
Dan Chaltiel <dan.chaltiel@gmail.com>
2023-05-17 17:03:42 +0200

    skip doppelganger tests

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,22 +1,14 @@
 
-skip_on_cran()
 
 # swimmerplot ---------------------------------------------------------------------------------
-
 
 test_that("swimmerplot", {
   e = edc_example_plot()
   load_list(e)
   
   p = swimmerplot(.lookup, plotly=FALSE)
-  vdiffr::expect_doppelganger("swimmerplot", p)
-  
   p2 = swimmerplot(.lookup, origin="db0$date_naissance", plotly=FALSE)
-  vdiffr::expect_doppelganger("swimmerplot with origin", p2)
-  
   p3 = swimmerplot(.lookup, group="db0$group", plotly=FALSE)
-  vdiffr::expect_doppelganger("swimmerplot with group", p3)
-  
   
   expect_error(swimmerplot(.lookup, origin="aaaaa", plotly=FALSE), 
                class="edc_swimplot_parse")
@@ -29,5 +21,11 @@ test_that("swimmerplot", {
                class="edc_swimplot_parse")
   expect_error(swimmerplot(.lookup, group="db1$x", plotly=FALSE), 
                class="edc_swimplot_group_dup")
+  
+  skip_on_cran()
+  skip_on_ci()
+  vdiffr::expect_doppelganger("swimmerplot", p)
+  vdiffr::expect_doppelganger("swimmerplot with origin", p2)
+  vdiffr::expect_doppelganger("swimmerplot with group", p3)
 })
 
