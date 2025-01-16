842ef4aacd328ac12baffa343474b8144c775fa6
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-01-16 21:28:54 +0100

    modularized edc_swimmerplot()

--- R/swimmerplot.R
+++ R/swimmerplot.R
@@ -7,8 +7,7 @@
 #' 
 #' Join all tables from `.lookup$dataset` on `id` 
 #'
-#' @param .lookup the lookup table, default to `edc_lookup()`
-#' @param id the patient identifier. Will be coerced as numeric.
+#' @param id the patient identifier. Will be coerced as numeric if possible.
 #' @param group a grouping variable, given as "dataset$column"
 #' @param origin a variable to consider as time 0, given as "dataset$column"
 #' @param id_lim a numeric vector of length 2 providing the minimum and maximum `id` to subset on. 
@@ -16,6 +15,7 @@
 #' @param time_unit if `origin!=NULL`, the unit to measure time. One of `c("days", "weeks", "months", "years")`.
 #' @param aes_color either `variable` ("\{dataset\} - \{column\}") or `label` (the column label)
 #' @param plotly whether to use `{plotly}` to get an interactive plot
+#' @param .lookup deprecated
 #' @param ... not used
 #'
 #' @return either a `plotly` or a `ggplot`
@@ -43,77 +43,30 @@
 #' @importFrom stringr str_detect str_ends str_remove str_replace_all
 #' @importFrom tidyr pivot_longer
 #' @importFrom tidyselect matches
-edc_swimmerplot = function(.lookup=edc_lookup(), ..., 
-                           id=get_subjid_cols(), 
+edc_swimmerplot = function(..., 
                            group=NULL, origin=NULL, 
                            id_lim=NULL,
                            exclude=NULL,
+                           id=get_subjid_cols(), 
                            time_unit=c("days", "weeks", "months", "years"),
                            aes_color=c("variable", "label"), 
-                           plotly=getOption("edc_plotly", FALSE)){
+                           plotly=getOption("edc_plotly", FALSE),
+                           .lookup="deprecated"){
   check_dots_empty()
+  aes_color = match.arg(aes_color)
+  aes_label = if(aes_color=="variable") "label" else "variable"
   time_unit = match.arg(time_unit[1], c(time_unit, str_remove(time_unit, "s$")))
   if(!str_ends(time_unit, "s")) time_unit = paste0(time_unit, "s")
-  aes_color = match.arg(aes_color)
   parent = parent.frame()
-  if(is.null(.lookup)){
-    cli_abort("{.arg .lookup} should not be {.val NULL}")
-  }
   
-  dbs = .lookup$dataset %>%
-    set_names() %>% 
-    map(~get(.x, envir=parent))
-  if(length(dbs)==0){
-    cli_abort("Unexpected error, contact the developper")
-  }
-  
-  dbs = dbs %>% 
-    discard(~!any(id %in% names(.x)))
-  if(length(dbs)==0){
-    cli_abort(c("None of the datasets contains an identifier column", i="{.arg id}={.val {id}}"))
-  }
-  dbs = dbs %>% 
-    map(~.x %>% select(matches(id), where(is.Date)) %>% rename(id=1)) %>% 
-    discard(~ncol(.x)<2)
-  if(length(dbs)==0){
-    cli_abort(c("None of the datasets contains a date column"))
-  }
-  
-  dbs = dbs %>% 
-    imap(~{
-      xid = suppressWarnings(as.numeric(.x$id))
-      pb = is.na(xid)!=is.na(.x$id)
-      if(any(pb)) cli_warn(c("NAs introduced by coercion to numeric in {.val {.y}${id}}. Is {.arg id} set correctly?", 
-                             i="Problematic value{?s}: {.val { .x$id[pb]}}"))
-      .x$id = xid
-      .x
-    })
-  
-  dat = dbs %>% 
-    imap(~{
-      .x %>% 
-        pivot_longer(-id) %>% 
-        mutate(
-          label=unlist(get_label(.x)[name]) %||% name,
-          dataset=.y,
-          variable=paste0(toupper(dataset), " - ", toupper(name))
-        )
-      
-    }) %>% 
+  dat = get_datasets() %>% 
+    .discard_if_no_id(id=id) %>% 
+    .select_dates(id=id) %>% 
+    .pivot_dates(id=id) %>% 
     list_rbind() %>% 
-    mutate(date=value)
-  
-  if(!is.null(exclude)){
-    excl = tolower(paste(exclude, collapse="|")) %>% 
-      str_replace_all("\\$", "\\\\$")
-    dat = dat %>% 
-      filter(!str_detect(tolower(paste0(dataset, "$", name)), excl))
-  }
-  
-  if(!is.null(id_lim)){
-    if(!is.numeric(id_lim) && length(id_lim)!=2) cli_abort("{.arg id_lim} should be a numeric vector of length 2")
-    dat = dat %>% filter(between(id, id_lim[1], id_lim[2]))
-  }
+    .exclude_columns(exclude) %>% 
+    .parse_id_to_numeric(id=id, id_lim=id_lim)%>% 
+    arrange(variable)
   
   if(!is.null(group)){
     dat_group = parse_var(group, id, parent)
@@ -121,40 +74,27 @@ edc_swimmerplot = function(.lookup=edc_lookup(), ...,
       cli_abort("{.arg group} ({group}) should identify subjects ({id}) uniquely.", 
                 class="edc_swimplot_group_dup")
     }
-    
     dat = dat %>% left_join(dat_group, by="id")
   }
   
-  if(can_be_numeric(dat$id)) dat$id=as.numeric(dat$id)
-  else if(all(str_detect(dat$id, "\\d+"))){
-    if(is_installed("gtools")) dat=slice(dat, gtools::mixedorder(id))
-    else cli_warn(c("{.arg id} contains numbers, you will need the 
-                    {.pkg gtools} package to sort it properly.", 
-                    i='Run {.run utils::install.package("gtools")}'))
-  }
   tooltip = c("x", "y", "color", "label")
   x_label = "Calendar date"
+  aes_x = "date"
   if(!is.null(origin)){
     dat_origin = parse_var(origin, id, parent)
     values = c(days=1, weeks=7, months=365.24/12, years=365.24)
     dat = dat %>%
       left_join(dat_origin, by="id") %>% 
       mutate(
-        date = value,
-        value = as.double(value-origin, units="days") / values[time_unit]
+        time = as.double(date-origin, units="days") / values[time_unit]
       )
     x_label = glue("Date difference from `{origin}` (in {time_unit})")
     tooltip = c(tooltip, "date")
-  }
-  
-  aes_label = "variable"
-  if(aes_color=="variable"){ 
-    aes_label = "label"
+    aes_x = "time"
   }
   
   p = dat %>% 
-    mutate(id=as_factor(id)) %>% 
-    ggplot(aes(x=value, y=id, group=id, date=date)) + 
+    ggplot(aes(x=!!sym(aes_x), y=id, group=id, date=date)) + 
     aes(color=!!sym(aes_color), label=!!sym(aes_label)) +
     geom_line(na.rm=TRUE) +
     geom_point(na.rm=TRUE) +
@@ -171,6 +111,131 @@ edc_swimmerplot = function(.lookup=edc_lookup(), ...,
   
   p
 }
+
+
+
+# Utils -------------------------------------------------------------------
+
+
+#' @noRd
+#' @keywords internal
+.discard_if_no_id = function(datasets, id){
+  has_id = datasets %>% 
+    map(~tolower(names(.x))) %>% 
+    map(~intersect(.x, tolower(id))) %>% 
+    map_lgl(~length(.x)>0)
+  if(!any(has_id)){
+    cli_abort(c("None of the datasets contains an identifier column", 
+                i="{.arg id}={.val {id}}"), 
+              call=parent.frame())
+  }
+  datasets[has_id]
+}
+
+
+#' @noRd
+#' @keywords internal
+.select_dates = function(datasets, id){
+  data_dates = datasets %>% 
+    map(~{
+      .x %>% 
+        select(id=any_of2(id), where(is.Date)) %>% 
+        rename(id=1)
+    }) %>% 
+    discard(~ncol(.x)<2)
+  if(length(data_dates)==0){
+    cli_abort(c("None of the datasets contains a date column"), 
+              call=parent.frame())
+  }
+  data_dates
+}
+
+.pivot_dates = function(datasets, id){
+  datasets %>% 
+    imap(~{
+      .x %>% 
+        pivot_longer(-id, values_to="date") %>% 
+        mutate(
+          label=unlist(get_label(.x)[name]) %||% name,
+          dataset=.y,
+          variable=paste0(dataset, "$", name)
+        )
+    })
+}
+
+.exclude_columns = function(data, exclude) {
+  if(!is.null(exclude)){
+    excl = exclude %>% paste(collapse="|") %>% tolower() %>% 
+      str_replace_all("\\$", "\\\\$")
+    data = data %>% 
+      filter(!str_detect(tolower(variable), excl))
+  }
+  data
+}
+
+.parse_id_to_numeric = function (data, id, id_lim) {
+  if(is.null(id_lim)) id_lim =c(-Inf, Inf)
+  if(!is.numeric(id_lim) && length(id_lim)!=2) {
+    cli_abort("{.arg id_lim} should be a numeric vector of length 2.")
+  }
+  
+  if(can_be_numeric(data$id)) {
+    data = data %>% 
+      mutate(id = as.numeric(id)) %>% 
+      filter(id>=id_lim[1] & id<=id_lim[2])
+  } else if(all(str_detect(data$id, "\\d+"))){
+    if(is_installed("gtools")){
+      data = data %>% 
+        slice(gtools::mixedorder(id)) %>% 
+        mutate(id=as_factor(id))
+    } else {
+      cli_warn(c("{.arg id} contains numbers, you will need the 
+                    {.pkg gtools} package to sort it properly.", 
+                 i='Run {.run utils::install.package("gtools")}'),
+               call=parent.frame())
+    }
+  }
+  data
+}
+
+#' @importFrom cli cli_abort
+#' @importFrom dplyr rename select
+#' @importFrom rlang caller_arg
+#' @importFrom stringr str_detect str_split
+#' @importFrom tidyselect matches
+#' @noRd
+#' @keywords internal
+parse_var = function(input, id, env){
+  input_name = caller_arg(input)
+  if(!str_detect(input, "^.*\\$.*$")){
+    cli_abort(c(x="{.arg {input_name}} is not in the form `dataset$column`.", 
+                i="{.arg {input_name}} = {.val {input}}"), 
+              class="edc_swimplot_parse", 
+              call=parent.frame())
+  }
+  input2 = str_split(input, "\\$", 2)[[1]]
+  
+  if(!exists(input2[1], envir=env)){
+    cli_abort(c(x="{.arg {input_name}} is wrong: no dataset {.val {input2[1]}} was found.", 
+                i="{.arg {input_name}} = {.val {input}}"), 
+              class="edc_swimplot_parse_dataset", 
+              call=parent.frame())
+  }
+  
+  dat_input = get(input2[1], envir=env)
+  if(!input2[2] %in% names(dat_input)){
+    cli_abort(c(x="{.arg {input_name}} is wrong: no column {.val {input2[2]}} in dataset {.val {input2[1]}} was found.", 
+                i="{.arg {input_name}} = {.val {input}}"), 
+              class="edc_swimplot_parse_column", 
+              call=parent.frame())
+  }
+  
+  dat_input %>% 
+    select(matches(id), !!input_name:=!!input2[2]) %>%
+    rename(id=1)
+}
+
+
 
 
 # Helper ------------------------------------------------------------------
@@ -200,46 +265,4 @@ save_plotly = function(p, file, ...){
   wd = setwd(path_dir(file))
   on.exit(setwd(wd))
   htmlwidgets::saveWidget(p, file=basename(file), ...)
-}
-
-
-# Utils -------------------------------------------------------------------
-
-#' @importFrom cli cli_abort
-#' @importFrom dplyr rename select
-#' @importFrom rlang caller_arg
-#' @importFrom stringr str_detect str_split
-#' @importFrom tidyselect matches
-#' @noRd
-#' @keywords internal
-parse_var = function(input, id, env){
-  input_name = caller_arg(input)
-  
-  if(!str_detect(input, "^.*\\$.*$")){
-    cli_abort(c(x="{.arg {input_name}} is not in the form `dataset$column`.", 
-                i="{.arg {input_name}} = {.val {input}}"), 
-              class="edc_swimplot_parse", 
-              call=parent.frame())
-  }
-  input2 = str_split(input, "\\$", 2)[[1]]
-  
-  if(!exists(input2[1], envir=env)){
-    cli_abort(c(x="{.arg {input_name}} is wrong: no dataset {.val {input2[1]}} was found.", 
-                i="{.arg {input_name}} = {.val {input}}"), 
-              class="edc_swimplot_parse_dataset", 
-              call=parent.frame())
-  }
-  
-  dat_input = get(input2[1], envir=env)
-  
-  if(!input2[2] %in% names(dat_input)){
-    cli_abort(c(x="{.arg {input_name}} is wrong: no column {.val {input2[2]}} in dataset {.val {input2[1]}} was found.", 
-                i="{.arg {input_name}} = {.val {input}}"), 
-              class="edc_swimplot_parse_column", 
-              call=parent.frame())
-  }
-  
-  dat_input %>% 
-    select(matches(id), !!input_name:=!!input2[2]) %>%
-    rename(id=1)
 }
