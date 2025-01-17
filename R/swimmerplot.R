6ad83478329a0355e0cb72dd667ee9b6741f6afb
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-01-17 16:04:33 +0100

    fixed swimmerplot doc

--- R/swimmerplot.R
+++ R/swimmerplot.R
@@ -5,7 +5,9 @@
 
 #' Swimmer plot of all dates columns
 #' 
-#' Join all tables from `.lookup$dataset` on `id` 
+#' Join all tables on `id` with only date columns to build a ggplot (or a 
+#' plotly if `plotly=TRUE`) showing all dates for each patients. This allows 
+#' outliers to be easily identified.
 #'
 #' @param id the patient identifier. Will be coerced as numeric if possible.
 #' @param group a grouping variable, given as "dataset$column"
@@ -25,10 +27,10 @@
 #' #tm = read_trialmaster("filename.zip", pw="xx")
 #' tm = edc_example_plot()
 #' load_list(tm)
-#' p = edc_swimmerplot(.lookup, id_lim=c(5,45))
-#' p2 = edc_swimmerplot(.lookup, origin="db0$date_naissance", time_unit="weeks", 
+#' p = edc_swimmerplot(id_lim=c(5,45))
+#' p2 = edc_swimmerplot(origin="db0$date_naissance", time_unit="weeks", 
 #'                      exclude=c("DB1$DATE2", "db3$.*"))
-#' p3 = edc_swimmerplot(.lookup, group="db0$group", aes_color="label")
+#' p3 = edc_swimmerplot(group="db0$group", aes_color="label")
 #' \dontrun{
 #' #save the plotly plot as HTML to share it
 #' save_plotly(p, "edc_swimmerplot.html")
