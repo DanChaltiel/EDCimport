2f3cf94986e33dced758b81bfac43aa599ed7ced
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-01-17 22:29:45 +0100

    fixed swimmerplot example

--- R/swimmerplot.R
+++ R/swimmerplot.R
@@ -28,9 +28,9 @@
 #' tm = edc_example()
 #' load_list(tm)
 #' p = edc_swimmerplot(id_lim=c(5,45))
-#' p2 = edc_swimmerplot(origin="db0$date_naissance", time_unit="weeks", 
+#' p2 = edc_swimmerplot(origin="enrol$date_naissance", time_unit="weeks", 
 #'                      exclude=c("DB1$DATE2", "db3$.*"))
-#' p3 = edc_swimmerplot(group="db0$group", aes_color="label")
+#' p3 = edc_swimmerplot(group="enrol$group", aes_color="label")
 #' \dontrun{
 #' #save the plotly plot as HTML to share it
 #' save_plotly(p, "edc_swimmerplot.html")
