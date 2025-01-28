6e49e9748eca006b9b18f23aa1b60b1789cc02ed
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-01-28 14:55:02 +0100

    working version, still WIP

--- R/viewer.R
+++ R/viewer.R
@@ -2,11 +2,9 @@
 #TODO: height en % plutôt qu'en px?
 #TODO: layout plutôt que dom (mettre tous les widgets en haut si possible)
 
-edc_viewer_ui = function(){
-  datasets = get_datasets()
+edc_viewer_ui = function(datasets, lookup){
   datasets = datasets[rev(order(map_dbl(datasets, nrow)))]
   
-  lookup = edc_lookup()
   extraction = attr(lookup, "datetime_extraction")
   EDCimport_version = attr(lookup, "EDCimport_version")
   project_name = attr(lookup, "project_name")
@@ -20,7 +18,7 @@ edc_viewer_ui = function(){
   title = format_inline("{{EDCimport}} Data browsing {par_projname}{par_extraction}{par_version}")
   
   page_sidebar(
-    window_title=paste(project_name, " -EDCimport"),
+    window_title=paste(project_name, " - EDCimport"),
     title=title,
     height="100vh",
     sidebar = sidebar(
@@ -30,9 +28,7 @@ edc_viewer_ui = function(){
                     multiple=TRUE),
       ),
       card(
-        # fill=FALSE,
-        # height="60vh",
-        card_title("Select a dataset:"),
+        card_title("Select a dataset:", container = htmltools::h3),
         tags$ul(
           style = "list-style: none; padding: 0;overflow-y: auto; ",
           lapply(names(datasets), function(name) {
@@ -58,109 +54,114 @@ edc_viewer_ui = function(){
       card_body(
         DTOutput("table")
       )
-    )
+    ),
+    tags$head(tags$style('.card{overflow: visible !important;}'),
+              tags$style('.card-body{overflow: visible !important;}'))
   )
 }
 
 # Serveur
-edc_viewer_server = function(input, output, session) {
-  datasets = get_datasets()
-  datasets = datasets[rev(order(map_dbl(datasets, nrow)))]
-  dataset_selected = reactiveVal(NULL)
-  subjid_cols = get_subjid_cols()
-  ids = datasets %>% 
-    map(~select(.x, any_of2(subjid_cols))) %>% 
-    unlist() %>% unique() %>% sort()
-  if(can_be_numeric(ids)){
-    ids = as.numeric(ids) %>% unique() %>% sort()
-  }
-  
-  dataset_selected(names(datasets)[1]) #init: show first dataset
-  
-  observe({
-    lapply(names(datasets), function(name) {
-      observeEvent(input[[paste0("link_", name)]], {
-        dataset_selected(name)
+edc_viewer_server = function(datasets, lookup) {
+  .set_lookup(lookup, verbose=FALSE)
+  subjid_cols = get_subjid_cols(lookup)
+  datasets = datasets
+  function(input, output, session) {
+    # datasets = get_datasets()
+    datasets = datasets[rev(order(map_dbl(datasets, nrow)))]
+    dataset_selected = reactiveVal(NULL)
+    ids = datasets %>% 
+      map(~select(.x, any_of2(subjid_cols))) %>% 
+      unlist() %>% unique() %>% sort()
+    if(can_be_numeric(ids)){
+      ids = as.numeric(ids) %>% unique() %>% sort()
+    }
+    
+    dataset_selected(names(datasets)[1]) #init: show first dataset
+    
+    observe({
+      lapply(names(datasets), function(name) {
+        observeEvent(input[[paste0("link_", name)]], {
+          dataset_selected(name)
+        })
       })
     })
-  })
-  
-  observe({
-    updateSelectInput(session, "subjid_selected", choices=c("All", ids), 
-                      selected="All")
-  })
-  
-  observeEvent(input$reset_subjid, {
-    updateSelectInput(session, "subjid_selected", choices=c("All", ids), 
-                      selected="All")
-    session$sendCustomMessage(type = 'testmessage',
-                              message = 'Thank you for clicking')
-  })
-  
-  output$dataset_name = renderText({
-    # req(dataset_selected())
-    data = datasets[[dataset_selected()]]
-    glue("Dataset selected: {name} ({nrow(data)} x {ncol(data)})", name=dataset_selected())
-  })
-  
-  output$table = renderDT({
-    req(dataset_selected())
-    subjid_selected=input$subjid_selected
-    if(is.null(subjid_selected)) subjid_selected = "All"
-    if(length(subjid_selected)>1){
-      updateSelectInput(session, "subjid_selected", 
-                        selected=setdiff(subjid_selected, "All"))
-    }
-    # browser()
-    data = datasets[[dataset_selected()]] %>% 
-      relocate(any_of2(subjid_cols), .before=1) %>% 
-      arrange(pick(any_of2(subjid_cols))) %>% 
-      filter(if_any(any_of2(subjid_cols), 
-                    ~any(subjid_selected=="All")|.x %in% subjid_selected))
-    labels = map_chr(data, ~attr(.x, "label"))
     
-    # Génération des noms de colonnes avec attribut "title" pour le hover
-    colnames_with_hover =  map_chr(colnames(data), ~{
-      label = attr(data[[.x]], "label")
-      if (!is.null(label) && label != "") {
-        # sprintf('<span title="%s">%s</span>', label, .x)
-        glue('<span title="{label}">{.x}</span>')
-      } else {
-        .x
-      }
+    observe({
+      updateSelectInput(session, "subjid_selected", choices=c("All", ids), 
+                        selected="All")
     })
     
-    datatable(
-      data,
-      # rownames = FALSE,
-      selection = "none",
-      filter = "top",
-      # height = "80%",
-      # plugins = "ellipsis",
-      escape = FALSE, # Autorise HTML
-      colnames = colnames_with_hover, # Applique les noms HTML
-      options = lst(
-        pageLength = 15,
-        # dom = "tp",
-        # autoWidth = TRUE,
-        # scrollX = TRUE,
-        columnDefs = dt_ellipsis(data, n=10),
-        # columnDefs = list(list(
-        #   targets = unname(which(map_lgl(data, ~is.character(.x)||is.factor(.x)))),
-        #   render = JS("$.fn.dataTable.render.ellipsis(17, false )")
-        # ))
-      )
-    ) %>% 
-      # Applique le CSS pour limiter la hauteur des cellules
-      formatStyle(
-        columns = seq_along(colnames(data)),
-        `white-space` = "nowrap",
-        `text-overflow` = "ellipsis",
-        `overflow` = "hidden",
-        # `max-width` = "150px",
-        `height` = "20px"
-      )
-  })
+    observeEvent(input$reset_subjid, {
+      updateSelectInput(session, "subjid_selected", choices=c("All", ids), 
+                        selected="All")
+      session$sendCustomMessage(type = 'testmessage',
+                                message = 'Thank you for clicking')
+    })
+    
+    output$dataset_name = renderText({
+      # req(dataset_selected())
+      data = datasets[[dataset_selected()]]
+      glue("Dataset selected: {name} ({nrow(data)} x {ncol(data)})", name=dataset_selected())
+    })
+    
+    output$table = renderDT({
+      req(dataset_selected())
+      subjid_selected=input$subjid_selected
+      if(is.null(subjid_selected)) subjid_selected = "All"
+      if(length(subjid_selected)>1){
+        updateSelectInput(session, "subjid_selected", 
+                          selected=setdiff(subjid_selected, "All"))
+      }
+      # browser()
+      data = datasets[[dataset_selected()]] %>% 
+        relocate(any_of2(subjid_cols), .before=1) %>% 
+        arrange(pick(any_of2(subjid_cols))) %>% 
+        filter(if_any(any_of2(subjid_cols), 
+                      ~any(subjid_selected=="All")|.x %in% subjid_selected))
+      labels = map_chr(data, ~attr(.x, "label"))
+      
+      # Génération des noms de colonnes avec attribut "title" pour le hover
+      colnames_with_hover =  map_chr(colnames(data), ~{
+        label = attr(data[[.x]], "label")
+        if (!is.null(label) && label != "") {
+          glue('<span title="{label}">{.x}</span>')
+        } else {
+          .x
+        }
+      })
+      
+      datatable(
+        data,
+        # rownames = FALSE,
+        selection = "none",
+        filter = "top",
+        # height = "80%",
+        # plugins = "ellipsis",
+        escape = FALSE, # Autorise HTML
+        colnames = colnames_with_hover, # Applique les noms HTML
+        options = lst(
+          pageLength = 15,
+          # dom = "tp",
+          # autoWidth = TRUE,
+          # scrollX = TRUE,
+          columnDefs = dt_ellipsis(data, n=10),
+          # columnDefs = list(list(
+          #   targets = unname(which(map_lgl(data, ~is.character(.x)||is.factor(.x)))),
+          #   render = JS("$.fn.dataTable.render.ellipsis(17, false )")
+          # ))
+        )
+      ) %>% 
+        # Applique le CSS pour limiter la hauteur des cellules
+        formatStyle(
+          columns = seq_along(colnames(data)),
+          `white-space` = "nowrap",
+          `text-overflow` = "ellipsis",
+          `overflow` = "hidden",
+          # `max-width` = "150px",
+          `height` = "20px"
+        )
+    })
+  }
 }
 
 dt_ellipsis = function(data, n){
@@ -180,17 +181,19 @@ dt_ellipsis = function(data, n){
 #'
 #' @returns
 #' @export
-edc_viewer = function(background=FALSE){
-  check_installed("bslib")
-  check_installed("shiny")
-  check_installed("DT")
-  library(shiny)
-  # library(shinyWidgets)
-  library(bslib)
-  # library(shinyjs)
-  library(DT)
-  launch_shiny = function(){
-    app = shiny::shinyApp(EDCimport:::edc_viewer_ui, EDCimport:::edc_viewer_server)
+edc_viewer = function(background=T){
+  lookup = edc_lookup()
+  datasets = get_datasets()
+  launch_shiny = function(datasets, lookup){
+    suppressPackageStartupMessages({
+      library(shiny)
+      # library(shinyWidgets)
+      # library(shinyjs)
+      library(bslib)
+      library(DT)
+    })
+    app = shiny::shinyApp(EDCimport:::edc_viewer_ui(datasets, lookup), 
+                          EDCimport:::edc_viewer_server(datasets, lookup))
     shiny::runApp(app, launch.browser=FALSE, port=1231)
   }
   
@@ -199,7 +202,12 @@ edc_viewer = function(background=FALSE){
     brw = Sys.getenv("R_BROWSER")
     # x=callr::r_bg(launch_shiny, stdout="out", stderr="errors",
     #               package="EDCimport", env = c(R_BROWSER=brw))
-    process=callr::r_bg(launch_shiny, package="EDCimport")
+    process=callr::r_bg(launch_shiny, 
+                        args=list(datasets=datasets, lookup=lookup), 
+                        stdout="out", stderr="errors",
+                        # env = c(R_BROWSER=brw),
+                        # error="error",
+                        package="EDCimport")
     if (process$is_alive()) {
       message("Application lancée en arrière-plan.")
       browseURL("http://127.0.0.1:1231")
@@ -209,11 +217,13 @@ edc_viewer = function(background=FALSE){
     return(process)
   }
   
-  launch_shiny()
+  browseURL("http://127.0.0.1:1231")
+  launch_shiny(datasets, lookup)
   invisible()
 }
 
-# devtools::load_all(".");x=edc_viewer();x$read_error()
+# devtools::load_all(".");x=edc_viewer(TRUE);
+# cat(x$read_error())
 
 
 
