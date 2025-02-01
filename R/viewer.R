f9cea47d346ad8f3d456a9454ab43254b6b40b7d
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-02-01 13:58:02 +0100

    allow bad datasets in edc_viewer

--- R/viewer.R
+++ R/viewer.R
@@ -1,25 +1,15 @@
-#TODO: Afficher en rouge les datasets où SUBJID est absent
-#TODO: height en % plutôt qu'en px?
-#TODO: layout plutôt que dom (mettre tous les widgets en haut si possible)
-
-#'@noRd
-#'@examples
-#'import_to_list("#' @importFrom bslib card card_body sidebar")
-#'import_to_list("shiny actionButton selectInput actionLink tags textOutput")
-import_to_list = function(x){
-  x = str_remove(x, "#' @importFrom ") %>% stringr::str_split_1(" ")
-  paste0(x[-1], "=", x[1], "::", x[-1]) %>% paste(collapse=";")
-}
 
 
 #' @importFrom cli format_inline
 #' @importFrom purrr map_dbl
 edc_viewer_ui = function(datasets, lookup){
-  datasets = datasets[rev(order(map_dbl(datasets, nrow)))]
-  card=bslib::card;card_body=bslib::card_body;card_header=bslib::card_header;card_title=bslib::card_title;page_sidebar=bslib::page_sidebar;sidebar=bslib::sidebar
+  card=bslib::card;card_body=bslib::card_body;card_header=bslib::card_header;
+  card_title=bslib::card_title;page_sidebar=bslib::page_sidebar;sidebar=bslib::sidebar
   DTOutput=DT::DTOutput
-  actionButton=shiny::actionButton;selectInput=shiny::selectInput;actionLink=shiny::actionLink;tags=shiny::tags;textOutput=shiny::textOutput
+  actionButton=shiny::actionButton;selectInput=shiny::selectInput;actionLink=shiny::actionLink;
+  tags=shiny::tags;textOutput=shiny::textOutput
   
+  datasets = datasets[rev(order(map_dbl(datasets, ~nrow(.x) %0% -1)))]
   extraction = attr(lookup, "datetime_extraction")
   EDCimport_version = attr(lookup, "EDCimport_version")
   project_name = attr(lookup, "project_name")
@@ -81,16 +71,19 @@ edc_viewer_ui = function(datasets, lookup){
 #' @importFrom purrr map map_chr map_dbl
 edc_viewer_server = function(datasets, lookup) {
   datatable=DT::datatable;formatStyle=DT::formatStyle;renderDT=DT::renderDT
-  observe=shiny::observe;observeEvent=shiny::observeEvent;reactiveVal=shiny::reactiveVal;renderText=shiny::renderText;req=shiny::req;updateSelectInput=shiny::updateSelectInput
+  observe=shiny::observe;observeEvent=shiny::observeEvent;reactiveVal=shiny::reactiveVal;
+  renderText=shiny::renderText;req=shiny::req;updateSelectInput=shiny::updateSelectInput
   
   .set_lookup(lookup, verbose=FALSE)
   subjid_cols = get_subjid_cols(lookup)
   datasets = datasets
+  
   function(input, output, session) {
     # datasets = get_datasets()
-    datasets = datasets[rev(order(map_dbl(datasets, nrow)))]
+    datasets = datasets[rev(order(map_dbl(datasets, ~nrow(.x) %0% -1)))]
     dataset_selected = reactiveVal(NULL)
-    ids = datasets %>% 
+    ids = datasets %>%
+      keep(is.data.frame) %>% 
       map(~select(.x, any_of2(subjid_cols))) %>% 
       unlist() %>% unique() %>% sort()
     if(can_be_numeric(ids)){
@@ -108,19 +101,17 @@ edc_viewer_server = function(datasets, lookup) {
     })
     
     observe({
-      updateSelectInput(session, "subjid_selected", choices=c("All", ids), 
-                        selected="All")
+      updateSelectInput(session, "subjid_selected", choices=c(ids))
     })
     
     observeEvent(input$reset_subjid, {
       updateSelectInput(session, "subjid_selected", choices=c("All", ids), 
                         selected="All")
-      session$sendCustomMessage(type = 'testmessage',
-                                message = 'Thank you for clicking')
     })
     
     output$dataset_name = renderText({
       data = datasets[[dataset_selected()]]
+      if(is.null(data)) return(glue("{name}: Corrupted dataset", name=dataset_selected()))
       glue("Dataset selected: {name} ({nrow(data)} x {ncol(data)})", name=dataset_selected())
     })
     
@@ -132,13 +123,13 @@ edc_viewer_server = function(datasets, lookup) {
         updateSelectInput(session, "subjid_selected", 
                           selected=setdiff(subjid_selected, "All"))
       }
-      # browser()
+      if(is.null(datasets[[dataset_selected()]])) return(tibble())
       data = datasets[[dataset_selected()]] %>% 
         relocate(any_of2(subjid_cols), .before=1) %>% 
         arrange(pick(any_of2(subjid_cols))) %>% 
         filter(if_any(any_of2(subjid_cols), 
                       ~any(subjid_selected=="All")|.x %in% subjid_selected))
-      labels = map_chr(data, ~attr(.x, "label"))
+      labels = map_chr(data, ~attr(.x, "label") %0% NA)
       
       # Génération des noms de colonnes avec attribut "title" pour le hover
       colnames_with_hover =  map_chr(colnames(data), ~{
@@ -184,20 +175,6 @@ edc_viewer_server = function(datasets, lookup) {
   }
 }
 
-#' @importFrom glue glue
-#' @importFrom purrr map_lgl
-dt_ellipsis = function(data, n){
-  list(list(
-    targets = unname(which(map_lgl(data, ~is.character(.x)||is.factor(.x)))),
-    render = DT::JS(glue(
-      "function(data, type, row, meta) {{",
-      "if(data==null || data==undefined) return ' ';",
-      "console.log(data);",
-      "return type === 'display' && data.length > {n} ?",
-      "'<span title=\"' + data + '\">' + data.substr(0, {n}) + '...</span>' : data;",
-      "}}"))
-  ))
-}
 
 #' Shiny data explorer
 #' 
@@ -221,21 +198,28 @@ edc_viewer = function(background=TRUE){
   if(isTRUE(background)){
     check_installed("callr", "for `import_review()` to work in background")
     brw = Sys.getenv("R_BROWSER")
-    # x=callr::r_bg(launch_shiny, stdout="out", stderr="errors",
-    #               package="EDCimport", env = c(R_BROWSER=brw))
-    process=callr::r_bg(launch_shiny, 
-                        args=list(datasets=datasets, lookup=lookup), 
-                        stdout="out", stderr="errors",
-                        # env = c(R_BROWSER=brw),
-                        # error="error",
-                        package="EDCimport")
-    if (process$is_alive()) {
-      message("Application lancée en arrière-plan.")
+    
+    if(exists("process", envir=edcimport_env)) {
+      cli_inform("Killing previous background process (PID: {edcimport_env$process$get_pid()})")
+      edcimport_env$process$kill()
+    }
+    
+    edcimport_env$process = callr::r_bg(
+      launch_shiny, 
+      args=list(datasets=datasets, lookup=lookup), 
+      stdout="out", stderr="errors",
+      # env = c(R_BROWSER=brw),
+      # error="error",
+      package="EDCimport"
+    )
+    if(edcimport_env$process$is_alive()) {
+      cli_inform(c("Shiny app launched in the background (PID: {edcimport_env$process$get_pid()})",
+                   i="Browse at {.url http://127.0.0.1:1231}"))
       browseURL("http://127.0.0.1:1231")
     } else {
-      message("Échec du lancement de l'application.")
+      cli_inform(c(x="Error: Shiny app couldn't be launched"))
     }
-    return(process)
+    return(edcimport_env$process)
   }
   
   browseURL("http://127.0.0.1:1231")
@@ -248,3 +232,30 @@ edc_viewer = function(background=TRUE){
 
 
 
+# Utils ---------------------------------------------------------------------------------------
+
+
+#'@noRd
+#'@examples
+#'import_to_list("#' @importFrom bslib card card_body sidebar")
+#'import_to_list("shiny actionButton selectInput actionLink tags textOutput")
+import_to_list = function(x){
+  x = str_remove(x, "#' @importFrom ") %>% stringr::str_split_1(" ")
+  paste0(x[-1], "=", x[1], "::", x[-1]) %>% paste(collapse=";")
+}
+
+
+#' @importFrom glue glue
+#' @importFrom purrr map_lgl
+dt_ellipsis = function(data, n){
+  list(list(
+    targets = unname(which(map_lgl(data, ~is.character(.x)||is.factor(.x)))),
+    render = DT::JS(glue(
+      "function(data, type, row, meta) {{",
+      "if(data==null || data==undefined) return ' ';",
+      "console.log(data);",
+      "return type === 'display' && data.length > {n} ?",
+      "'<span title=\"' + data + '\">' + data.substr(0, {n}) + '...</span>' : data;",
+      "}}"))
+  ))
+}
