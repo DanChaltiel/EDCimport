8df8b50f8eddf98b09bed778ae00759aa5dfbd2c
Dan Chaltiel <15105152+DanChaltiel@users.noreply.github.com>
2025-06-06 11:37:41 +0200

    can_be_numeric: add wrong values as attribute

--- R/assertions.R
+++ R/assertions.R
@@ -121,8 +121,9 @@ can_be_numeric = function(x){
   if(length(x)==0) return(NA)
   if(is.factor(x)) return(FALSE)
   stopifnot(is.atomic(x) || is.list(x))
-  xnum_na <- suppressWarnings(is.na(as.numeric(x)))
-  all(is.na(x)==xnum_na)
+  xnum_na = suppressWarnings(is.na(as.numeric(x)))
+  wrong = x[is.na(x)!=xnum_na]
+  structure(length(wrong)==0, wrong=wrong)
 }
 
 
